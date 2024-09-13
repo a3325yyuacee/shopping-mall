b53e17aef824d4644aa9bbf01e91edf49b87fd81
BOT <a3325yyuacee@gmail.com>
2024-09-13 20:11:35 +0800

    商品功能 - Refactor category , 使用 Enum 類型

---
+++ src/main/java/com/bot/shopping_mall/constant/ProductCategory.java
@@ -0,0 +1,8 @@
+package com.bot.shopping_mall.constant;
+
+public enum ProductCategory {
+
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- src/main/java/com/bot/shopping_mall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/bot/shopping_mall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.bot.shopping_mall.rowmapper;
 
+import com.bot.shopping_mall.constant.ProductCategory;
 import com.bot.shopping_mall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,10 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+        //透過Enum 清楚分類
+        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
