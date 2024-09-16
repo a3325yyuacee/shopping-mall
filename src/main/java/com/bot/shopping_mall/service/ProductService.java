3492a36433ee3573c701651a01380c195b15ad6f
BOT <a3325yyuacee@gmail.com>
2024-09-16 21:34:00 +0800

    商品功能 - 實作查詢商品列表

--- src/main/java/com/bot/shopping_mall/service/ProductService.java
+++ src/main/java/com/bot/shopping_mall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.bot.shopping_mall.service;
 import com.bot.shopping_mall.dto.ProductRequest;
 import com.bot.shopping_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
